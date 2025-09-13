f89f36bb5dc26077e2abe16f5c9b6f2cbd507e6c
ARUK <ayilvarrajkumar@gmail.com>
2025-09-14 01:22:18 +0530

    project initialized

---
+++ src/test/java/com/indiatourism/tourism_api_backend/TourismApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indiatourism.tourism_api_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourismApiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
